8e29b2ef207f14508975640ef3bd965c2a662a98
Damini9431 <160881312+Damini9431@users.noreply.github.com>
2024-09-28 22:56:21 +0530

    Add files via upload

--- QueueInterfaceDemo/PriorityQueueEx.java
+++ QueueInterfaceDemo/PriorityQueueEx.java
@@ -1,1 +1,21 @@
+package com.QueueInterfaceDemo;
+import java.util.PriorityQueue;
+public class PriorityQueueEx {
 
+	public static void main(String[] args) {
+     PriorityQueue<String> p=new PriorityQueue();
+     p.add("Java");
+     p.add("C");
+     p.add("SQL");
+     p.add("Python");
+     p.add("HTML");
+     p.offer("CSS");
+     System.out.println(p);
+     System.out.println(p.size());
+     System.out.println(p.remove());
+     System.out.println(p.poll());
+     System.out.println(p.element());
+     System.out.println(p.peek());
+	}
+
+}
